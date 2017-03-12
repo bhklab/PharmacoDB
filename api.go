2f7f022356a93b6fc5b9c95562dd22ce0ba791ec
assefamaru <me@alexandermaru.com>
2017-03-11 19:33:55 -0500

    fix query strings

--- api.go
+++ api.go
@@ -45,45 +45,32 @@ func getCell(c *gin.Context) {
 	defer db.Close()
 
 	id := c.Param("id")
-	row := db.QueryRow("select cell_id, accession_id, cell_name, tissue_name from cells inner join tissues on cells.tissue_id = tissues.tissue_id where cells.cell_id = ?;", id)
-
-	var err error
-
-	err = row.Scan(&cell.Id, &cell.Accession, &cell.Name, &cell.Tissue)
+	row := db.QueryRow("select cell_id, accession_id, cell_name, tissue_name from cells inner join tissues on cells.tissue_id = tissues.tissue_id where cells.cell_name = ?;", id)
+	err := row.Scan(&cell.Id, &cell.Accession, &cell.Name, &cell.Tissue)
 	if err != nil {
-		row := db.QueryRow("select cell_id, accession_id, cell_name, tissue_name from cells inner join tissues on cells.tissue_id = tissues.tissue_id where cells.cell_id = ?;", id)
+		row = db.QueryRow("select cell_id, accession_id, cell_name, tissue_name from cells inner join tissues on cells.tissue_id = tissues.tissue_id where cells.cell_id = ?;", id)
 		err = row.Scan(&cell.Id, &cell.Accession, &cell.Name, &cell.Tissue)
-		if err != nil {
-			row := db.QueryRow("select cell_id, accession_id, cell_name, tissue_name from cells inner join tissues on cells.tissue_id = tissues.tissue_id where cells.cell_id = ?;", id)
-			err = row.Scan(&cell.Id, &cell.Accession, &cell.Name, &cell.Tissue)
-			if err != nil {
-				result = gin.H{
-					"category": "cell line",
-					"count":    0,
-					"data":     nil,
-				}
-			} else {
-				result = gin.H{
-					"category": "cell line",
-					"count":    1,
-					"data":     cell,
-				}
-			}
-		} else {
-			result = gin.H{
-				"category": "cell line",
-				"count":    1,
-				"data":     cell,
-			}
+	}
+	if err != nil {
+		row = db.QueryRow("select cell_id, accession_id, cell_name, tissue_name from cells inner join tissues on cells.tissue_id = tissues.tissue_id where cells.accession_id = ?;", id)
+		err = row.Scan(&cell.Id, &cell.Accession, &cell.Name, &cell.Tissue)
+	}
+
+	if err != nil {
+		result = gin.H{
+			"category": "cell line",
+			"count":    0,
+			"data":     nil,
 		}
+		c.JSON(http.StatusNotFound, result)
 	} else {
 		result = gin.H{
 			"category": "cell line",
 			"count":    1,
 			"data":     cell,
 		}
+		c.JSON(http.StatusOK, result)
 	}
-	c.JSON(http.StatusOK, result)
 }
 
 func main() {
