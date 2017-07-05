2723c338b3dda0270e8e6445069fe51aa9b57102
assefamaru <me@alexandermaru.com>
2017-07-05 14:12:15 -0400

    add datasets index handler

--- datasets.go
+++ datasets.go
@@ -1,6 +1,12 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
 
 // Dataset is a dataset datatype.
 type Dataset struct {
@@ -8,7 +14,90 @@ type Dataset struct {
 	Name string `json:"name"`
 }
 
-// IndexDataset ...
+// IndexDataset returns a list of all datasets currently in database.
 func IndexDataset(c *gin.Context) {
+	var (
+		dataset  Dataset
+		datasets []Dataset
+	)
 
+	db, err := initDB()
+	defer db.Close()
+	if err != nil {
+		handleError(c, nil, http.StatusInternalServerError, "Internal Server Error")
+		return
+	}
+
+	all := c.DefaultQuery("all", "false")
+	if isTrue, _ := strconv.ParseBool(all); isTrue {
+		rows, er := db.Query("SELECT dataset_id, dataset_name FROM datasets;")
+		defer rows.Close()
+		if er != nil {
+			handleError(c, er, http.StatusInternalServerError, "Internal Server Error")
+			return
+		}
+		for rows.Next() {
+			err = rows.Scan(&dataset.ID, &dataset.Name)
+			if err != nil {
+				handleError(c, err, http.StatusInternalServerError, "Internal Server Error")
+				return
+			}
+			datasets = append(datasets, dataset)
+		}
+		c.IndentedJSON(http.StatusOK, gin.H{
+			"data":        datasets,
+			"total":       len(datasets),
+			"description": "List of all datasets in PharmacoDB",
+		})
+		return
+	}
+
+	curPage := c.DefaultQuery("page", "1")
+	perPage := c.DefaultQuery("per_page", "30")
+
+	page, err := strconv.Atoi(curPage)
+	if err != nil {
+		handleError(c, err, http.StatusInternalServerError, "Internal Server Error")
+		return
+	}
+	limit, err := strconv.Atoi(perPage)
+	if err != nil {
+		handleError(c, err, http.StatusInternalServerError, "Internal Server Error")
+		return
+	}
+
+	s := (page - 1) * limit
+	query := fmt.Sprintf("SELECT SQL_CALC_FOUND_ROWS dataset_id, dataset_name FROM datasets limit %d,%d;", s, limit)
+	rows, err := db.Query(query)
+	defer rows.Close()
+	if err != nil {
+		handleError(c, err, http.StatusInternalServerError, "Internal Server Error")
+		return
+	}
+	for rows.Next() {
+		err = rows.Scan(&dataset.ID, &dataset.Name)
+		if err != nil {
+			handleError(c, err, http.StatusInternalServerError, "Internal Server Error")
+			return
+		}
+		datasets = append(datasets, dataset)
+	}
+	row := db.QueryRow("SELECT FOUND_ROWS();")
+	var total int
+	err = row.Scan(&total)
+	if err != nil {
+		handleError(c, err, http.StatusInternalServerError, "Internal Server Error")
+		return
+	}
+
+	// Write pagination links in response header.
+	writeHeaderLinks(c, "/datasets", page, total, limit)
+
+	c.IndentedJSON(http.StatusOK, gin.H{
+		"data":        datasets,
+		"page":        page,
+		"per_page":    limit,
+		"total":       total,
+		"description": "List of all datasets in PharmacoDB",
+	})
 }
