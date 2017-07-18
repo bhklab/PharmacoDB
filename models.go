197c963258d5564fad45092a4a5f24c893606a64
assefamaru <me@alexandermaru.com>
2017-07-17 22:16:40 -0400

    Add data models for cell_lines, tissues, drugs, datasets, experiments, dose_responses, annotations.

--- models.go
+++ models.go
@@ -1,3 +1,70 @@
 package main
 
-// for storing data on cell struct, tissue struct ..., experiments struct
+// Cell is model for a cell line.
+type Cell struct {
+	ID          int         `json:"id"`
+	ACC         *string     `json:"accession_id,omitempty"`
+	Name        string      `json:"name"`
+	Tissue      *Tissue     `json:"tissue,omitempty"`
+	Annotations Annotations `json:"annotations,omitempty"`
+}
+
+// Cells is a collection of Cell.
+type Cells []Cell
+
+// Tissue is model for a tissue.
+type Tissue struct {
+	ID          int         `json:"id"`
+	Name        *string     `json:"name,omitempty"`
+	Annotations Annotations `json:"annotations,omitempty"`
+}
+
+// Tissues is a collection of Tissue.
+type Tissues []Tissue
+
+// Drug is model for a drug.
+type Drug struct {
+	ID          int         `json:"id"`
+	Name        string      `json:"name"`
+	Annotations Annotations `json:"annotations,omitempty"`
+}
+
+// Drugs is a collection of Drug.
+type Drugs []Drug
+
+// Dataset is model for a dataset.
+type Dataset struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+// Datasets is a collection of Dataset.
+type Datasets []Dataset
+
+// Experiment is model for an experiment.
+type Experiment struct {
+	ID            int           `json:"experiment_id"`
+	Cell          Cell          `json:"cell_line"`
+	Tissue        Tissue        `json:"tissue"`
+	Drug          Drug          `json:"drug"`
+	Dataset       Dataset       `json:"dataset"`
+	DoseResponses DoseResponses `json:"dose_responses,omitempty"`
+}
+
+// DoseResponse is model for dose/response data.
+type DoseResponse struct {
+	Dose     float64 `json:"dose"`
+	Response float64 `json:"response"`
+}
+
+// DoseResponses is a collection of DoseResponse.
+type DoseResponses []DoseResponse
+
+// Annotation is model for names used by various datasets for each model.
+type Annotation struct {
+	Name     string   `json:"name"`
+	Datasets Datasets `json:"datasets"`
+}
+
+// Annotations is a collection of Annotation.
+type Annotations []Annotation
