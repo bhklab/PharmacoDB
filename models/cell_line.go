c42b539ea659aac157f32954e5a48310c35a9dba
assefamaru <me@alexandermaru.com>
2017-07-20 18:49:51 -0400

    Add model structures

--- models/cell_line.go
+++ models/cell_line.go
@@ -1,1 +1,13 @@
 package models
+
+// Cell is model for a cell line.
+type Cell struct {
+	ID          int         `json:"id"`
+	ACC         string      `json:"accession_id,omitempty"`
+	Name        string      `json:"name"`
+	Tissue      Tissue      `json:"tissue,omitempty"`
+	Annotations Annotations `json:"annotations,omitempty"`
+}
+
+// Cells is a collection of Cell.
+type Cells []Cell
--- models/tissue.go
+++ models/tissue.go
@@ -1,1 +1,11 @@
 package models
+
+// Tissue is model for a tissue.
+type Tissue struct {
+	ID          int         `json:"id"`
+	Name        *string     `json:"name,omitempty"`
+	Annotations Annotations `json:"annotations,omitempty"`
+}
+
+// Tissues is a collection of Tissue.
+type Tissues []Tissue
