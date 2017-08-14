a675f4c0c9628093230317c1d84a477fd89722a4
assefamaru <me@alexandermaru.com>
2017-08-14 15:09:13 -0400

    update server + add utils module

--- api/server.go
+++ api/server.go
@@ -1,6 +1,23 @@
 package api
 
-// Init new server, using gin router.
-func Init(c *Config) {
+import "github.com/gin-gonic/gin"
 
+// Init new server.
+func Init(config *Config) {
+	// Gin router with default middleware: logger and recovery
+	router := gin.Default()
+
+	// Set database credentials
+	SetDB(config.Version)
+
+	v := router.Group(config.Version + "/")
+	for _, route := range routes {
+		v.Handle(route.Method, route.Endpoint, route.Handler)
+	}
+
+	// If no routers match the request url, return code 400 (Bad Request)
+	router.NoRoute(func(c *gin.Context) {})
+
+	// Listen and serve on config port
+	router.Run(":" + config.Port)
 }
