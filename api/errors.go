b0baa35c24c2470e25b76871ea599382319abc60
assefamaru <me@alexandermaru.com>
2017-08-16 14:04:33 -0400

    Update comments

--- api/errors.go
+++ api/errors.go
@@ -7,7 +7,7 @@ func init() {
 	raven.SetDSN("https://71d8d1bc8e4843eeba979fdaadebe48b:df30d2048fc44b5185809f04ba9d2294@sentry.io/186627")
 }
 
-// LogSentry submits private errors to sentry for internal error logging.
+// LogSentry submits private errors to sentry.
 func LogSentry(err error) {
 	raven.CaptureError(err, nil)
 }
--- api/server.go
+++ api/server.go
@@ -2,20 +2,20 @@ package api
 
 import "github.com/gin-gonic/gin"
 
-// Init new server.
+// Init server.
 func Init(config *Config) {
-	// Gin router with default middleware: logger and recovery
-	router := gin.Default()
-
 	// Set database credentials
 	SetDB(config.Version)
+
+	// Gin router with default middleware: logger and recovery
+	router := gin.Default()
 
 	v := router.Group(config.Version + "/")
 	for _, route := range routes {
 		v.Handle(route.Method, route.Endpoint, route.Handler)
 	}
 
-	// If no routers match the request url, return code 400 (Bad Request)
+	// If no routers match the request url, return 400 (Bad Request)
 	router.NoRoute(func(c *gin.Context) {})
 
 	// Listen and serve on config port
